3ba425f56a76ad64e65830943cd94da5b4a4b4c7
ShivShrivas <shivamsavita2000@gmail.com>
2024-03-19 11:23:23 +0530

    Signup page done

--- app/src/main/java/com/paisalo/sanskritsewaappandroid/LoginActivity.java
+++ app/src/main/java/com/paisalo/sanskritsewaappandroid/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.paisalo.sanskritsewaappandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        findViewById(R.id.loginButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,SignUpActivity.class));
+            }
+        });
     }
 }
